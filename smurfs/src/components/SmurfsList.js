95a4ef37c12ff0b955e2ed38e715a8fd8d0388dc
Tatiana Zhizhimontova <tatyana.zhi@gmail.com>
2020-05-29 18:48:52 -0700

    Get data request is created

--- smurfs/src/components/SmurfsList.js
+++ smurfs/src/components/SmurfsList.js
@@ -2,25 +2,19 @@ import React, { useEffect } from "react";
 
 import { connect } from "react-redux";
 import { getData } from "../actions";
-import SmurfsForm from "./SmurfsForm";
 
-const SmurfsList = () => {
+const SmurfsList = (props) => {
+  useEffect(() => {
+    props.getData();
+  }, []);
+  console.log("smurfs in SmurfsList", props.smurfs);
   return <div>SmurfsList</div>;
 };
-export default SmurfsList;
+const mapStateToProps = (state) => {
+  return {
+    smurfs: state.smurfs,
+    error: state.error,
+  };
+};
 
-// const SmurfsList = () => {
-//   return
-//   <div>SmurfsList</div>;
-
-// };
-// export default SmurfsList;
-
-// const mapStateToProps = (state) => {
-//   return {
-//     smurfs: state.smurfs,
-//     error: state.error,
-//   };
-// };
-
-// export default connect(mapStateToProps, { getData })(SmurfsList);
+export default connect(mapStateToProps, { getData })(SmurfsList);
